35881ba6eac62b2e9cd32b85214a8e93b97a9803
Abrahão Neto <abrahao-neto@outlook.com>
2022-07-12 19:00:02 -0300

    treinando

--- treinar/treinar/Program.cs
+++ treinar/treinar/Program.cs
@@ -7,6 +7,23 @@ namespace treinar
     {
         static void Main(string[] args)
         {
+            //01 - Percorrer o array e exibir quais são os pares e impares.
+
+            //02 - Retorna uma lista de números positivos, negativos.
+
+            //03 - Retornar números múltiplos de 11.
+
+            //04 - Retorna os numeros pares negativos.
+
+            //05 - Retornar os números impares positivos.
+
+            //06 - Informe se a posição corrente é maior que a proxima posição.
+
+            //07 - Informe qual é o maior valor da lista.
+
+            //08 - Informe qual é o menor valor impar da lista.
+
+
             //var valores = new Int32[20] { -15, -2, 21, -8, 59, -81, 3, 4, -36, 7, 5, 92, -1, -78, -9, -59, 82, -3, -46, 38 };
 
             //for (int i = 0; i < valores.Length; i++)
